1e80826e5c19ccc07376b94f47bbdb69110c4711
FangtaiDong10 <fangtaidong10@gmail.com>
2022-06-30 22:04:10 +1000

    form action/recommend and post method

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+const fs = require("fs");
 const express = require("express");
 const path = require("path");
 
 const app = express();
 
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
@@ -18,6 +20,23 @@ app.get("/restaurants", function (req, res) {
 app.get("/recommend", function (req, res) {
   const htmlFile = path.join(__dirname, "views", "recommend.html");
   res.sendFile(htmlFile);
+});
+
+app.post("/recommend", function (req, res) {
+  const restaurant = req.body;
+  const filePath = path.join(__dirname, "data", "restaurants.json");
+
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
+
+  storedRestaurants.push(restaurant);
+
+  // write back to the txt file
+  fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
+
+  // do redirect
+  res.redirect("/confirm");
+  
 });
 
 app.get("/confirm", function (req, res) {
